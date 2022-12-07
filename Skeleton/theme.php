dc8dfc3ad5197394e5379e31671cfb6277538c29
Selçuk Çukur <5716652+selcukcukur@users.noreply.github.com>
2022-12-07 20:40:32 +0300

    Required variables for `WiseCP` are included.

--- Skeleton/theme.php
+++ Skeleton/theme.php
@@ -13,5 +13,39 @@
  */
 class Skeleton_Theme
 {
-  // ...
+    /**
+     * The directory name where the theme files are located.
+     *
+     * @var string
+     */
+    public $name = 'Skeleton';
+
+    /**
+     * Customized configuration values for the theme.
+     *
+     * @var array
+     */
+    public $config = [];
+
+    /**
+     * The variable to use for exceptions that occur for the theme.
+     *
+     * @var mixed
+     */
+    public $error = null;
+
+    /**
+     * Default language lines customized for the theme.
+     *
+     * @var array
+     */
+    public $language;
+
+    /**
+     * Array where all available translations for the theme are stored.
+     *
+     * @var array
+     */
+    public $languages;
+
 }

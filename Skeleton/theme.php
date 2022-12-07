e8fd58ebd4b6dd0740e7717e0acc051edc156fa8
Selçuk Çukur <5716652+selcukcukur@users.noreply.github.com>
2022-12-07 20:27:53 +0300

    `theme.php` file has been created.

---
+++ Skeleton/theme.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Skeleton - The ultimate symphony for web art architecture!
+ *
+ * @package     Theme
+ * @subpackage  Skeleton
+ * @version     1.0.0
+ * @author      Selçuk Çukur <hk@selcukcukur.com.tr>
+ * @copyright   (C) 2010 - 2022 Example Company Inc.
+ *
+ * @see https://docs.example.com/ Skeleton : Docs
+ */
+class Skeleton_Theme
+{
+  // ...
+}

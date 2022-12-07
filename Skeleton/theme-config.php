5e4b61c59be3d54cba8cfec31bce09337b25f631
Selçuk Çukur <5716652+selcukcukur@users.noreply.github.com>
2022-12-07 20:12:36 +0300

    `theme-config.php` file has been created.

---
+++ Skeleton/theme-config.php
@@ -0,0 +1,48 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Theme General Information
+    |--------------------------------------------------------------------------
+    |
+    | You can edit the general fields of the theme, which includes many fields 
+    | such as your theme's name, developer, link to reach the developer, owner 
+    | and link to reach the owner.
+    |
+    */
+    'meta'                  => [
+        'name'          => 'WiseCP Theme Skeleton',
+        'description'   => 'Skeleton that you can use during theme development for WiseCP.',
+        'version'       => '1.0.0',
+        'provider'      => 'Selçuk Çukur',
+        'website'       => 'https://selcukcukur.com.tr',
+        'image'         => 'Cover.png',
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Theme Version URL
+    |--------------------------------------------------------------------------
+    |
+    | In order to easily manage the versions of your theme remotely, you can
+    | easily perform all updates and upgrades by typing the address where the
+    | theme "json" file is located.
+    |
+    */
+    'checking-version-url'  => '',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Theme Custom Settings
+    |--------------------------------------------------------------------------
+    |
+    | You may want to provide customizable fields for your theme, or you may
+    | want to create optional parts such as color, shape and custom fields.
+    | Everything you define in this field should come as an array in settings.
+    |
+    */
+    'settings'             => [
+        // ...
+    ],
+];
